dcb555e8ad09226f1c0d6718ca638e6f25b26b63
vrushal <vrushalnedungadi@gmail.com>
2025-04-04 12:34:53 -0700

    add tab

--- Category9/asvs9.1.py
+++ Category9/asvs9.1.py
@@ -1,10 +1,10 @@
-from burp import IBurpExtender, IContextMenuFactory, IScanIssue
+from burp import IBurpExtender, IContextMenuFactory, IScanIssue, ITab
 from javax import swing
 import subprocess
 import json
 
 
-class BurpExtender(IBurpExtender, IContextMenuFactory):
+class BurpExtender(IBurpExtender, IContextMenuFactory, ITab):
 
     def registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
@@ -14,46 +14,90 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         self._callbacks.registerContextMenuFactory(self)
         self._callbacks.issueAlert("TLS Scanner Extension Loaded!")
 
+        self._panel = swing.JPanel()
+        self._panel.setLayout(swing.BoxLayout(self._panel, swing.BoxLayout.Y_AXIS))
+
+        self._results_area = swing.JTextArea(15, 80)
+        self._results_area.setEditable(False)
+        scroll = swing.JScrollPane(self._results_area)
+        self._panel.add(scroll)
+
+        callbacks.addSuiteTab(self)
+
+
     def createMenuItems(self, invocation):
         menu = []
         menu_item = swing.JMenuItem("Scan TLS with sslyze")
-        menu_item.addActionListener(lambda event: self.scan())
+
+        def doScan(event):
+            try:
+                request = invocation.getSelectedMessages()[0]
+                http_service = request.getHttpService()
+                host = http_service.getHost()
+                self.scan(host)
+            except Exception as e:
+                self._callbacks.issueAlert("Error: " + str(e))
+
+
+        menu_item.addActionListener(doScan)
         menu.append(menu_item)
         return menu
+    
+    def getTabCaption(self):
+        return "TLS Security Summary"
+    
+    def getUiComponent(self):
+        return self._panel
+    
 
     
 
 
-    def scan(self):
+    def scan(self, host):
         
         try:
-            host = swing.JOptionPane.showInputDialog("Enter host (e.g., example.com):")
+            #host = swing.JOptionPane.showInputDialog("Enter host (e.g., example.com):")
             output = subprocess.check_output(["python", "sslyzefin.py", host], universal_newlines=True)
-            self._callbacks.issueAlert(output)
+           # self._callbacks.issueAlert(output)
             result = json.loads(output)
+
+            summary = "TLS Security scan for " + host + ": \n\n"
 
             if result:
 
                 if result["tls1.0"]:
-                    self._callbacks.issueAlert("TLS 1.0 connected! Not recommended!")
+                    summary +="TLS 1.0 connected! Not recommended!\n\n"
+                else:
+                    summary +="TLS 1.0 rejected.\n\n"
                 
                 if result["tls1.1"]:
-                    self._callbacks.issueAlert("TLS 1.1 connected! Not recommended!")
+                    summary +="TLS 1.1 connected! Not recommended!\n\n"
+                else:
+                    summary +="TLS 1.1 rejected.\n\n"
                 
                 if len(result["tls_1_2_bad"]) > 0:
+                    summary += "TLS1.2 accepted unsafe ciphersuites:\n\n"
                     for cipher in result["tls_1_2_bad"]:
                         message = cipher + " accepted. " + cipher + " not recommended."
-                        self._callbacks.issueAlert(message)
+                        summary += message + "\n\n"
+                else:
+                    summary += "Unsafe TLS1.2 ciphersuites rejected!\n\n"
+                    
 
                 if len(result["tls_1_3_bad"]) > 0:
+                    summary += "TLS1.3 accepted unsafe ciphersuites:\n\n"
                     for cipher in result["tls_1_3_bad"]:
                         message = cipher + " accepted. " + cipher + " not recommended."
-                        self._callbacks.issueAlert(message)
+                        summary += message + "\n\n"
+                else:
+                    summary += "Unsafe TLS1.3 ciphersuites rejected!\n\n"
+            self._results_area.setText(summary)
 
 
 
         except Exception as ex:
             self._callbacks.issueAlert(str(ex))
+            self._results_area.setText("Scan failed:\n" + str(ex))
         
 
 class CustomScanIssue(IScanIssue):
